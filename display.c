c35ecb5c32ea9ac0e8bf9061bcd6d125b3aa2e67
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-17 13:29:40 +0200

    feat(list): display list in frame algo on the go

--- display.c
+++ display.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/16 17:51:21 by abarthel          #+#    #+#             */
-/*   Updated: 2020/05/17 13:24:40 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/05/17 13:29:09 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,16 +52,27 @@ void	display_elements(struct s_select *data)
 {
 	struct s_element	*l;
 	int	i;
+	int	j;
+	int	width;
 
 	tputs(tgoto(data->termcaps.cm, 1, 1), 1, output);
 	l = data->elements;
 	i = 0;
+	j = 0;
+	width = 0;
 	while (l)
 	{
-		tputs(tgoto(data->termcaps.cm, 1, 1 + i), 1, output);
+		tputs(tgoto(data->termcaps.cm, 1 + j, 1 + i), 1, output);
 		ft_printf("%s%s%s", l->color, l->arg, DEFAULT);
+		if (l->len > width)
+			width = l->len + 1;
 		l = l->next;
 		++i;
+		if (i >= data->win.ws_row - 3)
+		{
+			i = 0;
+			j += width;
+		}
 	}
 }
 
