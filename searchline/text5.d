7e1b3f5f3baa9bc124eb17bcfa1c437ba5feb31e
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-31 12:16:36 +0200

    feat(search): simplifying search input

--- searchline/text5.d
+++
@@ -1,8 +0,0 @@
-searchline/text5.o: searchline/text5.c searchline/searchline.h \
- searchline/../select.h 42Libc/include/libft.h
-
-searchline/searchline.h:
-
-searchline/../select.h:
-
-42Libc/include/libft.h:
