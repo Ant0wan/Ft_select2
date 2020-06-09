9aae6c7f29554aeef3af22932918343c3eacdbd9
Antoine Barthelemy <abarthel@student.42.fr>
2020-06-09 19:36:52 +0200

    fix(exit)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/16 13:07:38 by abarthel          #+#    #+#             */
-/*   Updated: 2020/06/09 19:26:32 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/06/09 19:36:31 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
 		set_signals();
 		fill_elements(data, argc, argv);
 		data->cursor = data->elements;
-		//sort(data);
+		sort(data);
 
 		while (!acting)
 		{
