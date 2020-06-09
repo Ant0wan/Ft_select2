d57f08fac2d8103e11214be69331b367ca4f92fa
Antoine Barthelemy <abarthel@student.42.fr>
2020-06-09 19:35:57 +0200

    fix(exit)

--- selection.c
+++ selection.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 13:16:44 by abarthel          #+#    #+#             */
-/*   Updated: 2020/06/08 13:23:14 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/06/09 19:35:33 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 void	display_selection(struct s_select *data)
 {
+	struct	s_element	*e;
 	int	has_prev;
 
 	has_prev = 0;
-	while (data->elements)
+	e = data->elements;
+	while (e)
 	{
-		if (!has_prev && data->elements->selected)
+		if (!has_prev && e->selected)
 		{
 			has_prev = 1;
-			ft_printf("%s", data->elements->arg);
+			ft_printf("%s", e->arg);
 		}
-		else if (has_prev && data->elements->selected)
-			ft_printf(" %s", data->elements->arg);
-		data->elements = data->elements->next;
+		else if (has_prev && e->selected)
+			ft_printf(" %s", e->arg);
+		e = e->next;
 	}
 }
