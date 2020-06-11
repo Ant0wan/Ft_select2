2e9e3f5c10eb6597f49c03b61714426251e0e1fc
Antoine Barthelemy <abarthel@student.42.fr>
2020-06-11 15:47:22 +0200

    feat(display): go display pages

--- display.c
+++ display.c
@@ -6,11 +6,41 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/16 17:51:21 by abarthel          #+#    #+#             */
-/*   Updated: 2020/06/10 17:27:13 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/06/11 15:44:17 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "searchline.h"
+
+void	display_element(struct s_select *data, struct s_element *l, int w_frame)
+{
+	if (l == data->cursor)
+	{
+		if (!l->selected)
+		{
+			ft_dprintf(data->fd, "%s%s%.*s%s", l->color, UNDERL,
+				data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
+		}
+		else
+		{
+			ft_dprintf(data->fd, "%s%s%.*s%s", HIGHLI, UNDERL,
+				data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
+		}
+	}
+	else
+	{
+		if (!l->selected)
+		{
+			ft_dprintf(data->fd, "%s%.*s%s", l->color,
+				data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
+		}
+		else
+		{
+			ft_dprintf(data->fd, "%s%.*s%s", "\e[7m",
+				data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
+		}
+	}
+}
 
 void	display_elements(struct s_select *data)
 {
@@ -32,21 +62,7 @@ void	display_elements(struct s_select *data)
 	{
 		tputs(tgoto(data->termcaps.cm, w_frame + j, w_frame + i), 1, output);
 
-		if (l == data->cursor)
-		{
-			if (!l->selected)
-				ft_dprintf(data->fd, "%s%s%.*s%s", l->color, UNDERL, data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
-			else
-				ft_dprintf(data->fd, "%s%s%.*s%s", HIGHLI, UNDERL, data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
-		}
-		else
-		{
-			if (!l->selected)
-				ft_dprintf(data->fd, "%s%.*s%s", l->color, data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
-			else
-				ft_dprintf(data->fd, "%s%.*s%s", "\e[7m", data->win.ws_col - w_frame * 2, l->arg, DEFAULT);
-		}
-
+		display_element(data, l, w_frame);
 
 		if (l->len > width)
 			width = l->len + 1;
