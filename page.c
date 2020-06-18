373c2d3d783619f6e4a8902bf9f85202f86c51c1
Antoine Barthelemy <abarthel@student.42.fr>
2020-06-18 23:51:06 +0200

    norm(many): up to norm

--- page.c
+++ page.c
@@ -6,16 +6,17 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/03 17:32:47 by abarthel          #+#    #+#             */
-/*   Updated: 2020/06/18 18:18:36 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/06/18 23:51:01 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "select.h"
 
-static int	get_maxlen(struct s_select *data, int column_length, struct s_element *e)
+static int	get_maxlen(struct s_select *data, int column_length,
+		struct s_element *e)
 {
-	int maxlen;
-	int r;
+	int	maxlen;
+	int	r;
 
 	maxlen = 0;
 	r = data->frame_enabled / 2;
@@ -32,7 +33,8 @@ static int	get_maxlen(struct s_select *data, int column_length, struct s_element
 	return (maxlen);
 }
 
-static void	set_column_element(struct s_element **e, int maxlen, int column_length, int column_position)
+static void	set_column_element(struct s_element **e, int maxlen,
+		int column_length, int column_position)
 {
 	while (*e && column_length)
 	{
@@ -45,24 +47,25 @@ static void	set_column_element(struct s_element **e, int maxlen, int column_leng
 
 static void	set_page(struct s_select *data, struct s_element **e)
 {
-	int column_length;
-	int column_position;
-	int maxlen;
+	int	column_length;
+	int	column_position;
+	int	maxlen;
 
 	column_length = data->win.ws_row - data->frame_enabled - data->bar_enabled;
 	column_position = data->frame_enabled / 2;
 	if (column_length <= 0)
 		return ;
-
 	while (*e)
 	{
 		maxlen = get_maxlen(data, column_length, *e);
 		if (column_position != data->frame_enabled / 2)
-		{ // Case second column need check for space
-			if (column_position + maxlen + (data->frame_enabled / 2) > data->win.ws_col)
-				return;
+		{
+			if (column_position + maxlen + (data->frame_enabled / 2)
+				> data->win.ws_col)
+				return ;
 		}
-		maxlen = data->win.ws_col - (data->frame_enabled) < maxlen ? data->win.ws_col - (data->frame_enabled) : maxlen;
+		maxlen = data->win.ws_col - (data->frame_enabled) < maxlen ?
+			data->win.ws_col - (data->frame_enabled) : maxlen;
 		set_column_element(e, maxlen, column_length, column_position);
 		column_position += maxlen + 1;
 	}
@@ -79,15 +82,15 @@ static void	compute_pages(struct s_select *data)
 		++data->psum;
 		set_page(data, &e);
 	}
-
 }
 
-void	page(struct s_select *data)
+void		page(struct s_select *data)
 {
 	compute_pages(data);
 	if (data->psum > 1 && data->frame_enabled && data->win.ws_col > 10)
 	{
-		tputs(tgoto(data->termcaps.cm, data->win.ws_col / 2 - 5, data->win.ws_row - 2), 1, output);
+		tputs(tgoto(data->termcaps.cm, data->win.ws_col / 2 - 5,
+		data->win.ws_row - 2), 1, output);
 		ft_dprintf(data->fd, "%3d/%-3d", data->cursor->page, data->psum);
 	}
 	display_page(data);
